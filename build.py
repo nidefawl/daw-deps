0b2f714193363a19abad93585d721e6ce047104d
Michael Hept <michael.hept@gmail.com>
2022-02-05 14:00:11 +0100

    Update build script

--- build.py
+++ build.py
@@ -24,11 +24,12 @@ if len(sysargs) < 3:
 	sysargs = [__file__, "./build", "./install"]
 
 
-extraArgs = ""
+CMDLINE_EXTRA_ARGS = None
 if len(sysargs) > 3:
-    extraArgs=" "+(" ".join(sysargs[3:]))+" "
+    CMDLINE_EXTRA_ARGS=" "+(" ".join(sysargs[3:]))+" "
 
 PRINT_CMDS_ONLY = False
+# PRINT_CMDS_ONLY = True
 
 PATH_DEPS_REPO=script_path
 PATH_DEPS_BUILD_DIR = os.path.abspath(sysargs[1])
@@ -56,7 +57,7 @@ if COMPILER_NAME == "msvc":
 execution_environ = os.environ
 
 
-def buildLibrary(linkMode, name, extraArgs, optionalCmakeArgs=""):
+def buildLibrary(linkMode, name, cmakeConfig):
     BUILD_LOCATION = f'{PATH_DEPS_BUILD_DIR}{os.path.sep}build-{COMPILER_NAME}-{LINK_MODE_STRING[linkMode]}{os.path.sep}{name}'
     SRC_LOCATION = f'{PATH_DEPS_REPO}{os.path.sep}{name}'
 
@@ -65,8 +66,11 @@ def buildLibrary(linkMode, name, extraArgs, optionalCmakeArgs=""):
     
     SHARED_LIBS = 'Off' if linkMode==LINK_MODE_STATIC else 'On'
     
-    CMD_CMAKE_CONFIGURE = f'cmake {BUILD_FILE_GENERATOR} -S"{SRC_LOCATION}" -B"{BUILD_LOCATION}" -DBUILD_SHARED_LIBS:BOOL={SHARED_LIBS} {extraArgs} {optionalCmakeArgs}'
     
+    CMD_CMAKE_CONFIGURE = f'cmake {BUILD_FILE_GENERATOR} -S"{SRC_LOCATION}" -B"{BUILD_LOCATION}" -DBUILD_SHARED_LIBS:BOOL={SHARED_LIBS} '
+
+    if cmakeConfig: CMD_CMAKE_CONFIGURE += cmakeConfig
+    if CMDLINE_EXTRA_ARGS: CMD_CMAKE_CONFIGURE += CMDLINE_EXTRA_ARGS
     
     print("%s"%CMD_CMAKE_CONFIGURE)
     if not PRINT_CMDS_ONLY:
@@ -80,7 +84,8 @@ def buildLibrary(linkMode, name, extraArgs, optionalCmakeArgs=""):
         INSTALL_LOCATION = f'{PATH_DEPS_INSTALL_DIR}{os.path.sep}lib-{COMPILER_NAME}-{BUILD_TYPE_STRING[buildType].lower()}-{LINK_MODE_STRING[linkMode]}{os.path.sep}{name}'
         print("INSTALL_LOCATION %s"%INSTALL_LOCATION)
 
-        CMD_CMAKE_SET_INSTALL_LOCATION = f'cmake -DCMAKE_INSTALL_PREFIX:PATH="{INSTALL_LOCATION}" "{BUILD_LOCATION}"'
+        CMD_CMAKE_SET_INSTALL_LOCATION = f'cmake "{BUILD_LOCATION}" -DCMAKE_INSTALL_PREFIX:PATH="{INSTALL_LOCATION}" '
+
         print("%s"%CMD_CMAKE_SET_INSTALL_LOCATION)
         
         if not PRINT_CMDS_ONLY:
@@ -105,23 +110,59 @@ def buildLibrary(linkMode, name, extraArgs, optionalCmakeArgs=""):
 
 
 linkMode=LINK_MODE_STATIC
-buildLibrary(linkMode, "glfw" , extraArgs=extraArgs, optionalCmakeArgs=" -DGLFW_BUILD_DOCS:BOOL=OFF -DGLFW_BUILD_TESTS:BOOL=OFF -DGLFW_BUILD_EXAMPLES:BOOL=OFF")
 
-sqliteConfig = " -DSQLITECPP_RUN_CPPCHECK:BOOL=OFF -DSQLITECPP_RUN_CPPLINT:BOOL=OFF -DSQLITECPP_INTERNAL_SQLITE:BOOL=ON -DSQLITECPP_USE_STATIC_RUNTIME:BOOL=OFF -DSQLITECPP_USE_STACK_PROTECTION:BOOL=OFF"
-buildLibrary(linkMode, "SQLiteCpp" , extraArgs=extraArgs, optionalCmakeArgs = sqliteConfig)
+GLFW_ARGS=[
+  '',
+  'GLFW_BUILD_DOCS:BOOL=OFF',
+  'GLFW_BUILD_TESTS:BOOL=OFF',
+  'GLFW_BUILD_EXAMPLES:BOOL=OFF'
+]
+# buildLibrary(linkMode, "glfw" , ' -D'.join(GLFW_ARGS))
 
-buildLibrary(linkMode, "soxr" , extraArgs=extraArgs, optionalCmakeArgs=" -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTS:BOOL=OFF -DWITH_OPENMP:BOOL=OFF")
+SQLITECPP_ARGS=[
+  '',
+  'SQLITECPP_RUN_CPPCHECK:BOOL=OFF',
+  'SQLITECPP_RUN_CPPLINT:BOOL=OFF',
+  'SQLITECPP_INTERNAL_SQLITE:BOOL=ON',
+  'SQLITECPP_USE_STATIC_RUNTIME:BOOL=OFF',
+  'SQLITECPP_USE_STACK_PROTECTION:BOOL=OFF',
+]
+# buildLibrary(linkMode, "SQLiteCpp" , ' -D'.join(SQLITECPP_ARGS))
 
-portAudioConfig =" -DPA_DLL_LINK_WITH_STATIC_RUNTIME:BOOL=OFF -DPA_ENABLE_DEBUG_OUTPUT:BOOL=OFF"
+SOXR_ARGS=[
+  '',
+  'BUILD_EXAMPLES:BOOL=OFF',
+  'BUILD_TESTS:BOOL=OFF',
+  'WITH_OPENMP:BOOL=OFF',
+  'BUILD_SHARED_LIBS:BOOL=ON',
+ f'CMAKE_RELEASE_POSTFIX="-{COMPILER_NAME}-release"',
+ f'CMAKE_DEBUG_POSTFIX="-{COMPILER_NAME}-debug"',
+]
+buildLibrary(linkMode, "soxr" , ' -D'.join(SOXR_ARGS))
+
+PORTAUDIO_ARGS = [
+  '',
+ 'PA_DLL_LINK_WITH_STATIC_RUNTIME:BOOL=OFF',
+ 'PA_ENABLE_DEBUG_OUTPUT:BOOL=OFF'
+]
 if platform.system() == "Windows":
-  portAudioConfig +=" -DPA_USE_ASIO:BOOL=On"
+  PORTAUDIO_ARGS.append('PA_USE_ASIO:BOOL=ON')
 
-buildLibrary(linkMode, "portaudio" , extraArgs=extraArgs, optionalCmakeArgs = portAudioConfig)
+# buildLibrary(linkMode, "portaudio" , ' -D'.join(PORTAUDIO_ARGS))
 
-buildLibrary(linkMode, "portmidi", extraArgs=extraArgs, optionalCmakeArgs=" -DPM_USE_STATIC_RUNTIME=OFF")
+# buildLibrary(linkMode, "portmidi", " -DPM_USE_STATIC_RUNTIME=OFF")
 
-buildLibrary(linkMode, "pybind11", extraArgs=extraArgs, optionalCmakeArgs=" -DPYBIND11_TEST:BOOL=OFF -DPYBIND11_INSTALL:BOOL=On")
+# buildLibrary(linkMode, "pybind11", " -DPYBIND11_TEST:BOOL=OFF -DPYBIND11_INSTALL:BOOL=On")
 
-kissfftConfig = " -DKISSFFT_TEST=OFF -DKISSFFT_OPENMP=0 -DKISSFFT_DATATYPE=float -DKISSFFT_TOOLS=OFF -DKISSFFT_USE_ALLOCA=OFF -DKISSFFT_PKGCONFIG=OFF "
-kissfftConfig += " -DKISSFFT_STATIC=" + ('ON' if linkMode==LINK_MODE_STATIC else 'OFF')
-buildLibrary(linkMode, "kissfft", extraArgs=extraArgs, optionalCmakeArgs=kissfftConfig)
+KISSFFT_ARGS = [
+  '',
+  'KISSFFT_TEST=OFF',
+  'KISSFFT_OPENMP=0',
+  'KISSFFT_DATATYPE=float',
+  'KISSFFT_TOOLS=OFF',
+  'KISSFFT_USE_ALLOCA=OFF',
+  'KISSFFT_PKGCONFIG=OFF',
+  'KISSFFT_STATIC=' + ('ON' if linkMode==LINK_MODE_STATIC else 'OFF')
+]
+
+# buildLibrary(linkMode, "kissfft", ' -D'.join(KISSFFT_ARGS))
