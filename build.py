2ea3b42b8491fafe0b6db18445a13454eb50db32
nidefawl <637382+nidefawl@users.noreply.github.com>
2022-02-22 15:50:41 +0100

    Update build directory structure and build script

--- build.py
+++ build.py
@@ -1,20 +1,13 @@
+#!/usr/bin/env python3
+"""
+Dependencies install script
+USAGE: python daw-deps/build.py [build-directory] [install-path]
+"""
+
 import os
 import subprocess
-import errno
 import sys
 import platform
-
-
-# USAGE: python daw-deps/build.py [build-directory] [install-path]
-
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
 
 
 script_path = os.path.dirname(os.path.abspath(__file__))
@@ -36,22 +29,11 @@ PRINT_CMDS_ONLY = False
 PATH_DEPS_REPO = script_path
 PATH_DEPS_BUILD_DIR = os.path.abspath(sysargs[1])
 PATH_DEPS_INSTALL_DIR = os.path.abspath(sysargs[2])
-print('PATH_DEPS_REPO %s' % PATH_DEPS_REPO)
-print('PATH_DEPS_BUILD_DIR %s' % PATH_DEPS_BUILD_DIR)
-print('PATH_DEPS_INSTALL_DIR %s' % PATH_DEPS_INSTALL_DIR)
 
+print('PATH_DEPS_REPO', PATH_DEPS_REPO)
+print('PATH_DEPS_BUILD_DIR', PATH_DEPS_BUILD_DIR)
+print('PATH_DEPS_INSTALL_DIR', PATH_DEPS_INSTALL_DIR)
 
-BUILD_TYPE_RELEASE = 0
-BUILD_TYPE_DEBUG = 1
-LINK_MODE_STATIC = 0
-LINK_MODE_SHARED = 1
-
-# from pprint import pprint
-# cleanenv={}
-# for key,val in os.environ.items():
-#   cleanenv[key] = val
-# pprint(cleanenv)
-# sys.exit(0)
 
 IS_MSVC = 'VisualStudioVersion' in os.environ
 
@@ -65,14 +47,15 @@ if COMPILER_NAME == 'msvc':
 execution_environ = os.environ
 
 
-def buildLibrary(libraryName, cmakeConfig, buildConfigs=['Debug', 'Release']):
-
+def buildLibrary(libraryName, cmakeConfig, buildConfigs):
     BUILD_LOCATION = os.path.join(PATH_DEPS_BUILD_DIR, libraryName)
     SRC_LOCATION = f'{PATH_DEPS_REPO}{os.path.sep}{libraryName}'
 
     CMD_CMAKE_CONFIGURE = f'cmake {BUILD_FILE_GENERATOR} -S"{SRC_LOCATION}" -B"{BUILD_LOCATION}" '
     if len(buildConfigs) == 1:
         CMD_CMAKE_CONFIGURE += f' -DCMAKE_BUILD_TYPE={buildConfigs[0]}'
+    else:
+        CMD_CMAKE_CONFIGURE += ' -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -DCMAKE_DEBUG_POSTFIX=_debug -DCMAKE_RELEASE_POSTFIX=_release'
 
     if cmakeConfig:
         CMD_CMAKE_CONFIGURE += f' {cmakeConfig}'
@@ -81,41 +64,37 @@ def buildLibrary(libraryName, cmakeConfig, buildConfigs=['Debug', 'Release']):
     if CMDLINE_LOG_ARGS:
         CMD_CMAKE_CONFIGURE += f' {CMDLINE_LOG_ARGS}'
 
-    print('%s' % CMD_CMAKE_CONFIGURE)
+    print(CMD_CMAKE_CONFIGURE)
     if not PRINT_CMDS_ONLY:
-        mkdir_p(BUILD_LOCATION)
         ret = subprocess.call(CMD_CMAKE_CONFIGURE, stderr=subprocess.STDOUT, shell=True, env=execution_environ)
         if 0 != ret:
-            raise Exception('subprocess call returned %d' % ret)
+            raise Exception(f'subprocess call returned {ret}')
 
     for buildConfig in buildConfigs:
-        INSTALL_LOCATION = os.path.join(PATH_DEPS_INSTALL_DIR, libraryName)
-        if len(buildConfigs) > 1:
-            INSTALL_LOCATION = os.path.join(PATH_DEPS_INSTALL_DIR, libraryName, buildConfig.lower())
-        print('INSTALL_LOCATION %s' % INSTALL_LOCATION)
+        INSTALL_LOCATION = os.path.join(PATH_DEPS_INSTALL_DIR)
+        # if len(buildConfigs) > 1:
+        #     INSTALL_LOCATION = os.path.join(PATH_DEPS_INSTALL_DIR, libraryName, buildConfig.lower())
+        print('INSTALL_LOCATION', INSTALL_LOCATION)
         CMD_CMAKE_SET_INSTALL_LOCATION = f'cmake "{BUILD_LOCATION}" -DCMAKE_INSTALL_PREFIX:PATH="{INSTALL_LOCATION}" '
         if CMDLINE_LOG_ARGS:
             CMD_CMAKE_SET_INSTALL_LOCATION += f' {CMDLINE_LOG_ARGS}'
-        print('%s' % CMD_CMAKE_SET_INSTALL_LOCATION)
+        print(CMD_CMAKE_SET_INSTALL_LOCATION)
         if not PRINT_CMDS_ONLY:
-            mkdir_p(INSTALL_LOCATION)
             ret = subprocess.call(CMD_CMAKE_SET_INSTALL_LOCATION, stderr=subprocess.STDOUT, shell=True, env=execution_environ)
             if 0 != ret:
-                raise Exception('subprocess call returned %d' % ret)
+                raise Exception(f'subprocess call returned {ret}')
 
         CMD_CMAKE_BUILD_AND_INSTALL = f'cmake --build "{BUILD_LOCATION}" --config {buildConfig} --target install'
 
-        print('%s' % CMD_CMAKE_BUILD_AND_INSTALL)
+        print(CMD_CMAKE_BUILD_AND_INSTALL)
 
         if not PRINT_CMDS_ONLY:
             ret = subprocess.call(CMD_CMAKE_BUILD_AND_INSTALL, stderr=subprocess.STDOUT, shell=True, env=execution_environ)
             if 0 != ret:
-                raise Exception('subprocess call returned %d' % ret)
+                raise Exception(f'subprocess call returned {ret}')
 
         print('\n')
 
-
-linkMode = LINK_MODE_STATIC
 
 GLFW_ARGS = [
     '',
@@ -142,10 +121,11 @@ SOXR_ARGS = [
     'BUILD_EXAMPLES:BOOL=OFF',
     'BUILD_TESTS:BOOL=OFF',
     'WITH_OPENMP:BOOL=OFF',
+    'WITH_LSR_BINDINGS:BOOL=OFF',
+    'BUILD_SHARED_RUNTIME:BOOL=ON',
     'BUILD_SHARED_LIBS:BOOL=ON',
     f'CMAKE_RELEASE_POSTFIX="-{COMPILER_NAME}-release"',
-    f'CMAKE_DEBUG_POSTFIX="-{COMPILER_NAME}-debug"',
-    'BUILD_SHARED_LIBS:BOOL=ON'
+    f'CMAKE_DEBUG_POSTFIX="-{COMPILER_NAME}-debug"'
 ]
 buildLibrary("soxr", ' -D'.join(SOXR_ARGS), ['Release'])
 
@@ -172,7 +152,7 @@ KISSFFT_ARGS = [
     'KISSFFT_TOOLS=OFF',
     'KISSFFT_USE_ALLOCA=OFF',
     'KISSFFT_PKGCONFIG=OFF',
-    'KISSFFT_STATIC=' + ('ON' if linkMode == LINK_MODE_STATIC else 'OFF'),
+    'KISSFFT_STATIC=ON',
     'BUILD_SHARED_LIBS:BOOL=OFF'
 ]
 
@@ -185,4 +165,4 @@ BENCHMARK_ARGS = [
     'BUILD_SHARED_LIBS:BOOL=OFF'
 ]
 
-buildLibrary('google-benchmark', ' -D'.join(BENCHMARK_ARGS), ['Release'])
+buildLibrary('google-benchmark', ' -D'.join(BENCHMARK_ARGS), ['Release', 'Debug'])
